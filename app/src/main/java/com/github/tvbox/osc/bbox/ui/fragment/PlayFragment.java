1697c541585b773db8be9f9cda574288f26497ae
zbjaaa1 <168157190+zbjaaa1@users.noreply.github.com>
2025-02-28 13:14:27 +0800

    Update PlayFragment.java

--- app/src/main/java/com/github/tvbox/osc/bbox/ui/fragment/PlayFragment.java
+++ app/src/main/java/com/github/tvbox/osc/bbox/ui/fragment/PlayFragment.java
@@ -479,10 +479,16 @@ public class PlayFragment extends BaseLazyFragment {
     }
 
     void playUrl(String url, HashMap<String, String> headers) {
+        //playUrl:https:\/\/vv.jisuzyv.com\/play\/BeXN85gb\/index.m3u8  视频播放出错
+        //playUrl:https://svip.high21-playback.com/20250227/41759_2e1d6e6d/index.m3u8
+        url=url.trim().replace("\\","");
         LOG.i("playUrl:" + url);
+        /*
+        //404 Not Found
         if(autoRetryCount>0 && url.contains(".m3u8")){
             url="http://home.jundie.top:666/unBom.php?m3u8="+url;//尝试去bom头再次播放
         }
+        */
         if (mActivity == null) return;
         if (!isAdded()) return;
         mFinalUrl = url;
